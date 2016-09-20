600447501365e25a739e92a43a4f9a58ea5598b4
Dewwlo <a.bjrnlund@gmail.com>
2016-09-20 12:51:55 +0200

    rock, paper, shotgun

---
+++ Shotgun/Shotgun/Shotgun/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Shotgun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdLoad = new System.Windows.Forms.Button();
+            this.cmdBlock = new System.Windows.Forms.Button();
+            this.cmdFire = new System.Windows.Forms.Button();
+            this.cmdShotgun = new System.Windows.Forms.Button();
+            this.lblComputerAmmo = new System.Windows.Forms.Label();
+            this.lblComputerAmmoCurrent = new System.Windows.Forms.Label();
+            this.lblYourAmmoCurrent = new System.Windows.Forms.Label();
+            this.lblYourAmmo = new System.Windows.Forms.Label();
+            this.lblComputerLastAction = new System.Windows.Forms.Label();
+            this.lblYourLastAction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdLoad
+            // 
+            this.cmdLoad.Location = new System.Drawing.Point(115, 180);
+            this.cmdLoad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdLoad.Name = "cmdLoad";
+            this.cmdLoad.Size = new System.Drawing.Size(75, 58);
+            this.cmdLoad.TabIndex = 0;
+            this.cmdLoad.Text = "Load";
+            this.cmdLoad.UseVisualStyleBackColor = true;
+            this.cmdLoad.Click += new System.EventHandler(this.cmdLoad_Click);
+            // 
+            // cmdBlock
+            // 
+            this.cmdBlock.Location = new System.Drawing.Point(196, 180);
+            this.cmdBlock.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdBlock.Name = "cmdBlock";
+            this.cmdBlock.Size = new System.Drawing.Size(75, 58);
+            this.cmdBlock.TabIndex = 1;
+            this.cmdBlock.Text = "Block";
+            this.cmdBlock.UseVisualStyleBackColor = true;
+            this.cmdBlock.Click += new System.EventHandler(this.cmdBlock_Click);
+            // 
+            // cmdFire
+            // 
+            this.cmdFire.Enabled = false;
+            this.cmdFire.Location = new System.Drawing.Point(277, 180);
+            this.cmdFire.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdFire.Name = "cmdFire";
+            this.cmdFire.Size = new System.Drawing.Size(75, 58);
+            this.cmdFire.TabIndex = 2;
+            this.cmdFire.Text = "Fire";
+            this.cmdFire.UseVisualStyleBackColor = true;
+            this.cmdFire.Click += new System.EventHandler(this.cmdFire_Click);
+            // 
+            // cmdShotgun
+            // 
+            this.cmdShotgun.Location = new System.Drawing.Point(115, 244);
+            this.cmdShotgun.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdShotgun.Name = "cmdShotgun";
+            this.cmdShotgun.Size = new System.Drawing.Size(237, 64);
+            this.cmdShotgun.TabIndex = 3;
+            this.cmdShotgun.Text = "Shotgun!";
+            this.cmdShotgun.UseVisualStyleBackColor = true;
+            this.cmdShotgun.Visible = false;
+            this.cmdShotgun.Click += new System.EventHandler(this.cmdShotgun_Click);
+            // 
+            // lblComputerAmmo
+            // 
+            this.lblComputerAmmo.AutoSize = true;
+            this.lblComputerAmmo.Location = new System.Drawing.Point(74, 91);
+            this.lblComputerAmmo.Name = "lblComputerAmmo";
+            this.lblComputerAmmo.Size = new System.Drawing.Size(116, 17);
+            this.lblComputerAmmo.TabIndex = 4;
+            this.lblComputerAmmo.Text = "Computer Ammo:";
+            // 
+            // lblComputerAmmoCurrent
+            // 
+            this.lblComputerAmmoCurrent.AutoSize = true;
+            this.lblComputerAmmoCurrent.Location = new System.Drawing.Point(184, 91);
+            this.lblComputerAmmoCurrent.Name = "lblComputerAmmoCurrent";
+            this.lblComputerAmmoCurrent.Size = new System.Drawing.Size(16, 17);
+            this.lblComputerAmmoCurrent.TabIndex = 5;
+            this.lblComputerAmmoCurrent.Text = "0";
+            // 
+            // lblYourAmmoCurrent
+            // 
+            this.lblYourAmmoCurrent.AutoSize = true;
+            this.lblYourAmmoCurrent.Location = new System.Drawing.Point(354, 91);
+            this.lblYourAmmoCurrent.Name = "lblYourAmmoCurrent";
+            this.lblYourAmmoCurrent.Size = new System.Drawing.Size(16, 17);
+            this.lblYourAmmoCurrent.TabIndex = 7;
+            this.lblYourAmmoCurrent.Text = "0";
+            // 
+            // lblYourAmmo
+            // 
+            this.lblYourAmmo.AutoSize = true;
+            this.lblYourAmmo.Location = new System.Drawing.Point(274, 91);
+            this.lblYourAmmo.Name = "lblYourAmmo";
+            this.lblYourAmmo.Size = new System.Drawing.Size(85, 17);
+            this.lblYourAmmo.TabIndex = 6;
+            this.lblYourAmmo.Text = "Your Ammo:";
+            // 
+            // lblComputerLastAction
+            // 
+            this.lblComputerLastAction.AutoSize = true;
+            this.lblComputerLastAction.Location = new System.Drawing.Point(74, 125);
+            this.lblComputerLastAction.Name = "lblComputerLastAction";
+            this.lblComputerLastAction.Size = new System.Drawing.Size(137, 17);
+            this.lblComputerLastAction.TabIndex = 8;
+            this.lblComputerLastAction.Text = "Computer last action";
+            // 
+            // lblYourLastAction
+            // 
+            this.lblYourLastAction.AutoSize = true;
+            this.lblYourLastAction.Location = new System.Drawing.Point(274, 125);
+            this.lblYourLastAction.Name = "lblYourLastAction";
+            this.lblYourLastAction.Size = new System.Drawing.Size(106, 17);
+            this.lblYourLastAction.TabIndex = 9;
+            this.lblYourLastAction.Text = "Your last action";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 453);
+            this.Controls.Add(this.lblYourLastAction);
+            this.Controls.Add(this.lblComputerLastAction);
+            this.Controls.Add(this.lblYourAmmoCurrent);
+            this.Controls.Add(this.lblYourAmmo);
+            this.Controls.Add(this.lblComputerAmmoCurrent);
+            this.Controls.Add(this.lblComputerAmmo);
+            this.Controls.Add(this.cmdShotgun);
+            this.Controls.Add(this.cmdFire);
+            this.Controls.Add(this.cmdBlock);
+            this.Controls.Add(this.cmdLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdLoad;
+        private System.Windows.Forms.Button cmdBlock;
+        private System.Windows.Forms.Button cmdFire;
+        private System.Windows.Forms.Button cmdShotgun;
+        private System.Windows.Forms.Label lblComputerAmmo;
+        private System.Windows.Forms.Label lblComputerAmmoCurrent;
+        private System.Windows.Forms.Label lblYourAmmoCurrent;
+        private System.Windows.Forms.Label lblYourAmmo;
+        private System.Windows.Forms.Label lblComputerLastAction;
+        private System.Windows.Forms.Label lblYourLastAction;
+    }
+}
+
---
+++ Shotgun/Shotgun/Shotgun/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shotgun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int computerAmmo = 0;
+        int playerAmmo = 0;
+
+        // Action 1 = Load
+        // Action 2 = Block
+        // Action 3 = Fire
+        // Action 4 = Shotgun
+        int compChoice = 0;
+        int playerChoice = 0;
+
+        // result 0 Nothing happends
+        // result 1 Draw
+        // result 2 Computer Win
+        // result 3 Player win
+        int result = 0;
+        
+
+        private void cmdLoad_Click(object sender, EventArgs e)
+        {
+            playerChoice = 1;
+            ResolveTurn();
+        }
+
+        private void cmdBlock_Click(object sender, EventArgs e)
+        {
+            playerChoice = 2;
+            ResolveTurn();
+        }
+
+        private void cmdFire_Click(object sender, EventArgs e)
+        {
+            playerChoice = 3;
+            ResolveTurn();
+        }
+            
+        private void cmdShotgun_Click(object sender, EventArgs e)
+        {
+            playerChoice = 4;
+            ResolveTurn();
+        }
+
+        public void ResolveTurn()
+        {
+            ComputerAction();
+            UpdateLabelAmmoInfo();
+
+            if (playerChoice == 1)
+            {
+                switch (compChoice)
+                {
+                    case 3: result = 2;
+                        break;
+                    case 4: result = 2;
+                        break;
+                    default: result = 0;
+                        break;
+                }
+            }
+            else if (playerChoice == 2)
+            {
+                switch (compChoice)
+                {
+                    case 4: result = 2;
+                        break;
+                    default: result = 0;
+                        break;
+                }
+
+            }
+            else if (playerChoice == 3)
+            {
+                switch (compChoice)
+                {
+                    case 1: result = 3;
+                        break;
+                    case 2: result = 0;
+                        break;
+                    case 3: result = 0;
+                        break;
+                    case 4: result = 2;
+                        break;
+                }
+            }
+            else if (playerChoice == 4)
+            {
+                switch (compChoice)
+                {
+                    case 4: result = 1;
+                        break;
+                    default: result = 3;
+                        break;
+                }
+            }
+
+            EnableFire();
+            CheckWinner();
+        }
+
+        public void UpdateLabelAmmoInfo()
+        {
+            if (playerChoice == 1)
+            {
+                playerAmmo++;
+                lblYourLastAction.Text = "You load";
+                lblYourAmmoCurrent.Text = playerAmmo.ToString();
+            }
+            else if (playerChoice == 2)
+            {
+                lblYourLastAction.Text = "You block";
+            }
+            else if (playerChoice == 3)
+            {
+                playerAmmo--;
+                lblYourLastAction.Text = "You fire";
+                lblYourAmmoCurrent.Text = playerAmmo.ToString();
+            }
+            else if (playerChoice == 4)
+            {
+                lblYourLastAction.Text = "You used shotgun";
+            }
+        }
+
+        public void ComputerAction()
+        {
+            compChoice = CompRandomNumber();
+
+            if (compChoice == 1)
+            {
+                computerAmmo++;
+                lblComputerLastAction.Text = "Computer Loads";
+                lblComputerAmmoCurrent.Text = computerAmmo.ToString();
+            }
+            else if (compChoice == 2)
+            {
+                lblComputerLastAction.Text = "Computer Blocks";
+            }
+            else if (compChoice == 3)
+            {
+                computerAmmo--;
+                lblComputerLastAction.Text = "Computer Fires";
+                lblComputerAmmoCurrent.Text = computerAmmo.ToString();
+            }
+            else if (compChoice == 4)
+            {
+                lblComputerLastAction.Text = "Computer use Shotgun!";
+            }
+        }
+
+        public int CompRandomNumber()
+        {
+            int i = 1;
+
+            if (playerAmmo > 0 && computerAmmo == 0)
+            {
+                Random first = new Random();
+                i = first.Next(1, 3);
+            }
+            else if (computerAmmo > 0 && computerAmmo < 3)
+            {
+                Random second = new Random();
+                i = second.Next(1, 4);
+            }
+            else if (computerAmmo == 3)
+            {
+                i = 4;
+            }
+
+            return i;
+        }
+                
+        public void EnableShotgun()
+        {
+            if (playerAmmo == 3)
+            {
+                cmdShotgun.Visible = true;
+                cmdLoad.Visible = false;
+                cmdBlock.Visible = false;
+                cmdFire.Visible = false;
+            }
+            else
+            {
+
+            }
+        }
+
+        public void EnableFire()
+        {
+            if (playerAmmo > 0)
+            {
+                cmdFire.Enabled = true;
+            }
+            else
+            {
+                cmdFire.Enabled = false;
+            }
+        }
+
+        public void CheckWinner()
+        {
+            EnableShotgun();
+
+            if (result == 2)
+            {
+                MessageBox.Show("You are defeated!");
+                RestartOrExit();
+            }
+            else if (result == 3)
+            {
+                MessageBox.Show("You defeated your opponent!");
+                RestartOrExit();
+            }
+            else if (result == 1)
+            {
+                MessageBox.Show("The battle ended with a draw!");
+                RestartOrExit();
+            }
+        }
+
+        public void RestartOrExit()
+        {
+            DialogResult playAgain = MessageBox.Show("Do you want to play again?", "", MessageBoxButtons.YesNo);
+            if (playAgain == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start(Application.ExecutablePath);
+                Application.Exit();
+            }
+            else if (playAgain  == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
