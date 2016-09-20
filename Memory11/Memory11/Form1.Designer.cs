91cbe99726406bf75a19c5abdcdc57e839dc6987
Dewwlo <a.bjrnlund@gmail.com>
2016-09-20 14:19:46 +0200

    memory added

---
+++ Memory11/Memory11/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Memory11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdE1 = new System.Windows.Forms.Button();
+            this.cmdH1 = new System.Windows.Forms.Button();
+            this.cmdC1 = new System.Windows.Forms.Button();
+            this.cmdA1 = new System.Windows.Forms.Button();
+            this.cmdA2 = new System.Windows.Forms.Button();
+            this.cmdF1 = new System.Windows.Forms.Button();
+            this.cmdG1 = new System.Windows.Forms.Button();
+            this.cmdD1 = new System.Windows.Forms.Button();
+            this.cmdB1 = new System.Windows.Forms.Button();
+            this.cmdD2 = new System.Windows.Forms.Button();
+            this.cmdC2 = new System.Windows.Forms.Button();
+            this.cmdF2 = new System.Windows.Forms.Button();
+            this.cmdH2 = new System.Windows.Forms.Button();
+            this.cmdE2 = new System.Windows.Forms.Button();
+            this.cmdG2 = new System.Windows.Forms.Button();
+            this.cmdB2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdE1
+            // 
+            this.cmdE1.BackColor = System.Drawing.Color.Black;
+            this.cmdE1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdE1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdE1.Location = new System.Drawing.Point(59, 57);
+            this.cmdE1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdE1.Name = "cmdE1";
+            this.cmdE1.Size = new System.Drawing.Size(77, 78);
+            this.cmdE1.TabIndex = 0;
+            this.cmdE1.UseVisualStyleBackColor = false;
+            this.cmdE1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdH1
+            // 
+            this.cmdH1.BackColor = System.Drawing.Color.Black;
+            this.cmdH1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdH1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdH1.Location = new System.Drawing.Point(141, 57);
+            this.cmdH1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdH1.Name = "cmdH1";
+            this.cmdH1.Size = new System.Drawing.Size(77, 78);
+            this.cmdH1.TabIndex = 1;
+            this.cmdH1.UseVisualStyleBackColor = false;
+            this.cmdH1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdC1
+            // 
+            this.cmdC1.BackColor = System.Drawing.Color.Black;
+            this.cmdC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdC1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdC1.Location = new System.Drawing.Point(308, 57);
+            this.cmdC1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdC1.Name = "cmdC1";
+            this.cmdC1.Size = new System.Drawing.Size(77, 78);
+            this.cmdC1.TabIndex = 3;
+            this.cmdC1.UseVisualStyleBackColor = false;
+            this.cmdC1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdA1
+            // 
+            this.cmdA1.BackColor = System.Drawing.Color.Black;
+            this.cmdA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdA1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdA1.Location = new System.Drawing.Point(225, 57);
+            this.cmdA1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdA1.Name = "cmdA1";
+            this.cmdA1.Size = new System.Drawing.Size(77, 78);
+            this.cmdA1.TabIndex = 2;
+            this.cmdA1.UseVisualStyleBackColor = false;
+            this.cmdA1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdA2
+            // 
+            this.cmdA2.BackColor = System.Drawing.Color.Black;
+            this.cmdA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdA2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdA2.Location = new System.Drawing.Point(308, 137);
+            this.cmdA2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdA2.Name = "cmdA2";
+            this.cmdA2.Size = new System.Drawing.Size(77, 78);
+            this.cmdA2.TabIndex = 7;
+            this.cmdA2.UseVisualStyleBackColor = false;
+            this.cmdA2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdF1
+            // 
+            this.cmdF1.BackColor = System.Drawing.Color.Black;
+            this.cmdF1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdF1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdF1.Location = new System.Drawing.Point(225, 137);
+            this.cmdF1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdF1.Name = "cmdF1";
+            this.cmdF1.Size = new System.Drawing.Size(77, 78);
+            this.cmdF1.TabIndex = 6;
+            this.cmdF1.UseVisualStyleBackColor = false;
+            this.cmdF1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdG1
+            // 
+            this.cmdG1.BackColor = System.Drawing.Color.Black;
+            this.cmdG1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdG1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdG1.Location = new System.Drawing.Point(141, 137);
+            this.cmdG1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdG1.Name = "cmdG1";
+            this.cmdG1.Size = new System.Drawing.Size(77, 78);
+            this.cmdG1.TabIndex = 5;
+            this.cmdG1.UseVisualStyleBackColor = false;
+            this.cmdG1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdD1
+            // 
+            this.cmdD1.BackColor = System.Drawing.Color.Black;
+            this.cmdD1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdD1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdD1.Location = new System.Drawing.Point(59, 137);
+            this.cmdD1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdD1.Name = "cmdD1";
+            this.cmdD1.Size = new System.Drawing.Size(77, 78);
+            this.cmdD1.TabIndex = 4;
+            this.cmdD1.UseVisualStyleBackColor = false;
+            this.cmdD1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdB1
+            // 
+            this.cmdB1.BackColor = System.Drawing.Color.Black;
+            this.cmdB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdB1.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdB1.Location = new System.Drawing.Point(308, 217);
+            this.cmdB1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdB1.Name = "cmdB1";
+            this.cmdB1.Size = new System.Drawing.Size(77, 78);
+            this.cmdB1.TabIndex = 11;
+            this.cmdB1.UseVisualStyleBackColor = false;
+            this.cmdB1.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdD2
+            // 
+            this.cmdD2.BackColor = System.Drawing.Color.Black;
+            this.cmdD2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdD2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdD2.Location = new System.Drawing.Point(225, 217);
+            this.cmdD2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdD2.Name = "cmdD2";
+            this.cmdD2.Size = new System.Drawing.Size(77, 78);
+            this.cmdD2.TabIndex = 10;
+            this.cmdD2.UseVisualStyleBackColor = false;
+            this.cmdD2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdC2
+            // 
+            this.cmdC2.BackColor = System.Drawing.Color.Black;
+            this.cmdC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdC2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdC2.Location = new System.Drawing.Point(141, 217);
+            this.cmdC2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdC2.Name = "cmdC2";
+            this.cmdC2.Size = new System.Drawing.Size(77, 78);
+            this.cmdC2.TabIndex = 9;
+            this.cmdC2.UseVisualStyleBackColor = false;
+            this.cmdC2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdF2
+            // 
+            this.cmdF2.BackColor = System.Drawing.Color.Black;
+            this.cmdF2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdF2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdF2.Location = new System.Drawing.Point(59, 217);
+            this.cmdF2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdF2.Name = "cmdF2";
+            this.cmdF2.Size = new System.Drawing.Size(77, 78);
+            this.cmdF2.TabIndex = 8;
+            this.cmdF2.UseVisualStyleBackColor = false;
+            this.cmdF2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdH2
+            // 
+            this.cmdH2.BackColor = System.Drawing.Color.Black;
+            this.cmdH2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdH2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdH2.Location = new System.Drawing.Point(308, 297);
+            this.cmdH2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdH2.Name = "cmdH2";
+            this.cmdH2.Size = new System.Drawing.Size(77, 78);
+            this.cmdH2.TabIndex = 15;
+            this.cmdH2.UseVisualStyleBackColor = false;
+            this.cmdH2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdE2
+            // 
+            this.cmdE2.BackColor = System.Drawing.Color.Black;
+            this.cmdE2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdE2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdE2.Location = new System.Drawing.Point(225, 297);
+            this.cmdE2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdE2.Name = "cmdE2";
+            this.cmdE2.Size = new System.Drawing.Size(77, 78);
+            this.cmdE2.TabIndex = 14;
+            this.cmdE2.UseVisualStyleBackColor = false;
+            this.cmdE2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdG2
+            // 
+            this.cmdG2.BackColor = System.Drawing.Color.Black;
+            this.cmdG2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.cmdG2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdG2.Location = new System.Drawing.Point(141, 297);
+            this.cmdG2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdG2.Name = "cmdG2";
+            this.cmdG2.Size = new System.Drawing.Size(77, 78);
+            this.cmdG2.TabIndex = 13;
+            this.cmdG2.UseVisualStyleBackColor = false;
+            this.cmdG2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // cmdB2
+            // 
+            this.cmdB2.BackColor = System.Drawing.Color.Black;
+            this.cmdB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdB2.ForeColor = System.Drawing.Color.Transparent;
+            this.cmdB2.Location = new System.Drawing.Point(59, 297);
+            this.cmdB2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmdB2.Name = "cmdB2";
+            this.cmdB2.Size = new System.Drawing.Size(77, 78);
+            this.cmdB2.TabIndex = 12;
+            this.cmdB2.UseVisualStyleBackColor = false;
+            this.cmdB2.Click += new System.EventHandler(this.MatchLetter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 459);
+            this.Controls.Add(this.cmdB2);
+            this.Controls.Add(this.cmdH2);
+            this.Controls.Add(this.cmdE2);
+            this.Controls.Add(this.cmdG2);
+            this.Controls.Add(this.cmdB1);
+            this.Controls.Add(this.cmdD2);
+            this.Controls.Add(this.cmdC2);
+            this.Controls.Add(this.cmdF2);
+            this.Controls.Add(this.cmdA2);
+            this.Controls.Add(this.cmdF1);
+            this.Controls.Add(this.cmdG1);
+            this.Controls.Add(this.cmdD1);
+            this.Controls.Add(this.cmdC1);
+            this.Controls.Add(this.cmdA1);
+            this.Controls.Add(this.cmdH1);
+            this.Controls.Add(this.cmdE1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdE1;
+        private System.Windows.Forms.Button cmdH1;
+        private System.Windows.Forms.Button cmdC1;
+        private System.Windows.Forms.Button cmdA1;
+        private System.Windows.Forms.Button cmdA2;
+        private System.Windows.Forms.Button cmdF1;
+        private System.Windows.Forms.Button cmdG1;
+        private System.Windows.Forms.Button cmdD1;
+        private System.Windows.Forms.Button cmdB1;
+        private System.Windows.Forms.Button cmdD2;
+        private System.Windows.Forms.Button cmdC2;
+        private System.Windows.Forms.Button cmdF2;
+        private System.Windows.Forms.Button cmdH2;
+        private System.Windows.Forms.Button cmdE2;
+        private System.Windows.Forms.Button cmdG2;
+        private System.Windows.Forms.Button cmdB2;
+    }
+}
+
---
+++ Memory11/Memory11/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string checkFirst = "";
+        string buttonPressed = "";
+        int checkColor = 0;
+        string colorControl = "A";
+        string lastButtonName = "";
+
+        private void MatchLetter(object sender, EventArgs e)
+        {
+            var buttonName = (sender as Button).Name;
+            if (buttonName == lastButtonName)
+            {
+                return;
+            }
+            lastButtonName = buttonName;
+            GetButtonName(buttonName);
+            ShowColor(buttonName);
+
+
+            if (buttonPressed == "")
+            {
+                buttonPressed = checkFirst;
+                if (checkColor == 1)
+                {
+                    ResetColor(colorControl);
+                    checkColor = 0;
+                    ShowColor(buttonName);
+                }
+            }
+            else if (buttonPressed != "")
+            {
+                if (buttonPressed == checkFirst)
+                {
+                    HideLetter(buttonPressed);
+                    buttonPressed = "";
+                }
+                else if (buttonPressed != checkFirst)
+                {
+                    checkColor = 1;
+                    buttonPressed = "";
+                }
+            }
+        }
+        public void HideLetter(string letter)
+        {
+            switch (letter)
+            {
+                case "A": cmdA1.Visible = false; cmdA2.Visible = false; break;
+                case "B": cmdB1.Visible = false; cmdB2.Visible = false; break;
+                case "C": cmdC1.Visible = false; cmdC2.Visible = false; break;
+                case "D": cmdD1.Visible = false; cmdD2.Visible = false; break;
+                case "E": cmdE1.Visible = false; cmdE2.Visible = false; break;
+                case "F": cmdF1.Visible = false; cmdF2.Visible = false; break;
+                case "G": cmdG1.Visible = false; cmdG2.Visible = false; break;
+                case "H": cmdH1.Visible = false; cmdH2.Visible = false; break;
+            }
+        }
+        public void GetButtonName(string letter)
+        {
+            switch (letter)
+            {
+                case "cmdA1": checkFirst = "A"; break;
+                case "cmdB1": checkFirst = "B"; break;
+                case "cmdC1": checkFirst = "C"; break;
+                case "cmdD1": checkFirst = "D"; break;
+                case "cmdE1": checkFirst = "E"; break;
+                case "cmdF1": checkFirst = "F"; break;
+                case "cmdG1": checkFirst = "G"; break;
+                case "cmdH1": checkFirst = "H"; break;
+                case "cmdA2": checkFirst = "A"; break;
+                case "cmdB2": checkFirst = "B"; break;
+                case "cmdC2": checkFirst = "C"; break;
+                case "cmdD2": checkFirst = "D"; break;
+                case "cmdE2": checkFirst = "E"; break;
+                case "cmdF2": checkFirst = "F"; break;
+                case "cmdG2": checkFirst = "G"; break;
+                case "cmdH2": checkFirst = "H"; break;
+            }
+        }
+        public void ShowColor(string letter)
+        {
+            switch (letter)
+            {
+                case "cmdA1": cmdA1.BackColor = Color.Green; break;
+                case "cmdB1": cmdB1.BackColor = Color.Blue; break;
+                case "cmdC1": cmdC1.BackColor = Color.Yellow; break;
+                case "cmdD1": cmdD1.BackColor = Color.Red; break;
+                case "cmdE1": cmdE1.BackColor = Color.Tan; break;
+                case "cmdF1": cmdF1.BackColor = Color.Orange; break;
+                case "cmdG1": cmdG1.BackColor = Color.Purple; break;
+                case "cmdH1": cmdH1.BackColor = Color.Aqua; break;
+                case "cmdA2": cmdA2.BackColor = Color.Green; break;
+                case "cmdB2": cmdB2.BackColor = Color.Blue; break;
+                case "cmdC2": cmdC2.BackColor = Color.Yellow; break;
+                case "cmdD2": cmdD2.BackColor = Color.Red; break;
+                case "cmdE2": cmdE2.BackColor = Color.Tan; break;
+                case "cmdF2": cmdF2.BackColor = Color.Orange; break;
+                case "cmdG2": cmdG2.BackColor = Color.Purple; break;
+                case "cmdH2": cmdH2.BackColor = Color.Aqua; break;
+            }
+        }
+        public void ResetColor(string letter)
+        {
+            switch (letter)
+            {
+                case "A":   cmdA1.BackColor = Color.Black; 
+                            cmdB1.BackColor = Color.Black; 
+                            cmdC1.BackColor = Color.Black;
+                            cmdD1.BackColor = Color.Black;
+                            cmdE1.BackColor = Color.Black;
+                            cmdF1.BackColor = Color.Black;
+                            cmdG1.BackColor = Color.Black;
+                            cmdH1.BackColor = Color.Black;
+                            cmdA2.BackColor = Color.Black;
+                            cmdB2.BackColor = Color.Black;
+                            cmdC2.BackColor = Color.Black;
+                            cmdD2.BackColor = Color.Black;
+                            cmdE2.BackColor = Color.Black;
+                            cmdF2.BackColor = Color.Black;
+                            cmdG2.BackColor = Color.Black;
+                            cmdH2.BackColor = Color.Black; break;
+            }
+        }
+    }
+}
